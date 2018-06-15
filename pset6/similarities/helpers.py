ff0f9c7fe8d044e7d4a6f2d656b34e378bf3c73f
Voldemaras Slatkevicius <ultracurrency@gmail.com>
2018-06-15 09:13:32 +0000

    finished Problem set 6

--- pset6/similarities/helpers.py
+++ pset6/similarities/helpers.py
@@ -1,16 +1,19 @@
 from nltk.tokenize import sent_tokenize
 
+
 def compare(a, b):
     """Compare two lists and return final list without duplicates"""
     final = list(set(a) & set(b))
     return final
 
+
 def substringify(a, n):
     """Takes input as a string and returns list of items of lenght n"""
     final = []
     for index in range(len(a) - n + 1):
-        final.append(a[index:index+n])
+        final.append(a[index:index + n])
     return final
+
 
 def lines(a, b):
     """Return lines in both a and b"""
@@ -19,12 +22,14 @@ def lines(a, b):
 
     return compare(split1, split2)
 
+
 def sentences(a, b):
     """Return sentences in both a and b"""
     sent1 = sent_tokenize(a)
     sent2 = sent_tokenize(b)
 
     return compare(sent1, sent2)
+
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
